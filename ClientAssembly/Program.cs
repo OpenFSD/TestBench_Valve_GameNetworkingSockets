3584c321502b9ed84d062a004803722a72dddb57
Full Stack Development Template <Jasper.Assembly@icloud.com>
2025-03-25 12:41:11 +1000

    Add files via upload

---
+++ ClientAssembly/Program.cs
@@ -0,0 +1,15 @@
+﻿//ClientAssembly
+using System.Runtime.InteropServices;
+
+namespace Florence.ServerAssembly
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Florence.ClientAssembly.Networking.CreateNetworkingClient();
+            //Valve.Sockets.Native.GameNetworkingSockets_Init();
+        }
+
+    }
+}
